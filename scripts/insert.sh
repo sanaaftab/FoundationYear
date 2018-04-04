51430cfbf336da58b0691246161fcde729d6e8ca
Ben <Ben@Piggott.eu>
2018-04-04 13:27:21 +0100

    Added dynamic path
    Script is can now edit html documents in a different directory in any file structure as long as the scripts and html directories share the same parent directory

--- scripts/insert.sh
+++ scripts/insert.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 #Script to insert a header and a footer into a html document from a template
-head=$(sed '\,/head,q' template.html)
+
+#Retrieving current directory to work out location of html directory 
+locationHTML=$(pwd | sed 's_/*[^/]\+/*$_/html_')
+
+#Extracting header and footer from template document
+head=$(sed '\,/head,q' $locationHTML/template.html)
+foot=$(sed -n '/footer/,$p' $locationHTML/template.html)
+
+#Debugging output
+echo $head
+echo $foot
+echo $locationHTML
+
+#
+sed -i "1i $head" $locationHTML/$1	
